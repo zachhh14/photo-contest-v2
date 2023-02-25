803f6ca9a1ecc9958680d022057868be7813ae7c
zachhh14 <zactec115@gmail.com>
2023-02-25 23:42:42 +0800

    update index and app css and app.jsx to remove some of the templates, added navbar.jsx and navbar.css

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul className="nav-buttons">
+        <ol>HOME</ol>
+        <ol>FAQ</ol>
+        <ol>WINNERS</ol>
+        <ol>VOTE</ol>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
